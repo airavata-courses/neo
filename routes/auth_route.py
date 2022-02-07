7015ce5fa2e423cf7cff3797b2400db10b8ff29a
Nirav Raje <rajenirav@gmail.com>
2022-02-06 19:06:34 -0500

    Updated auth_route to handle tokenID and communicate with auth-service #13

--- routes/auth_route.py
+++ routes/auth_route.py
@@ -1,28 +1,29 @@
 from flask import Blueprint, jsonify, request
+from proto_converters import protobuf_to_dict
 import grpc
 import auth_pb2
 import auth_pb2_grpc
 
 auth_api = Blueprint('auth_api', __name__)
 
-# TBD: channel port number
-channel = grpc.insecure_channel('localhost:43000', options=(('grpc.enable_http_proxy', 0),))
+# Change hostname from "auth-service" to "localhost" if running without docker-compose
+channel = grpc.insecure_channel('auth-service:43000', options=(('grpc.enable_http_proxy', 0),))
 stub = auth_pb2_grpc.AuthStub(channel)
 
-# TBD: endpoint name
 @auth_api.route('/login', methods=["GET"])
 def login():
     if request.method == 'GET':
         tokenId = request.args.get('tokenId')
         if not tokenId:
             tokenId = " "
-
+        
         response = stub.loginUser(
             auth_pb2.LoginArgs(
                 tokenId=tokenId
             )
         )
-        return jsonify({"response": response}), 200
+        response_dict = protobuf_to_dict(response)
+        return jsonify(response_dict), 200
     else:
         return jsonify({"message": "Method Not Allowed"}), 405
 
