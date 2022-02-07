d11d94b05406ef8504ebd48234e8fdb867243386
Nirav Raje <rajenirav@gmail.com>
2022-02-07 03:35:42 -0500

    Removed dangling routes, registered metadata route, updated Pipfile and requirements.txt #13

--- app.py
+++ app.py
@@ -3,22 +3,26 @@ from flask_cors import CORS
 import requests
 
 # import blueprints (APIs) from routes
-from routes.data_processor_route import data_processor_api
+from routes.dnu_data_processor_route import data_processor_api
 from routes.registry_route import registry_api
 from routes.auth_route import auth_api
+from routes.metadata_route import metadata_api
 
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/', methods=["POST", "GET"])
 def check():
     print('Gateway is running...')
     return "Gateway Started!"
 
+
 app.register_blueprint(data_processor_api)
 app.register_blueprint(auth_api)
 app.register_blueprint(registry_api)
+app.register_blueprint(metadata_api)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='8081')
