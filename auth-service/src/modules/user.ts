881ae51674307386604a90aa2cd2120a4fcbe92a
Rajdeep Chauhan <rajdeepc2792@gmail.com>
2022-03-04 17:14:04 -0500

    udpated check token with custom jwt access

--- auth-service/src/modules/user.ts
+++ auth-service/src/modules/user.ts
@@ -31,20 +31,55 @@ export const getAuth2Client = () => {
 
 const checkToken = async (auth2Client, idToken) => {
     try {
-        const ticket = await auth2Client.verifyIdToken({
-            idToken,
-            audience: settings.CLIENT_ID
-        });
+        // const ticket = await auth2Client.verifyIdToken({
+        //     idToken,
+        //     audience: settings.CLIENT_ID
+        // });
 
-        const payload = ticket.getPayload();
+        // const payload = ticket.getPayload();
 
-        if (!payload['sub']) {
+        // if (!payload['sub']) {
+        //     return {
+        //         isAuth: false
+        //     };
+        // }
+
+        if(idToken == null) {
             return {
                 isAuth: false
             };
         }
 
-        return { ...ticket.getPayload(), isAuth: true };
+        const parts = idToken.split('.');
+        if (parts.length === 2){
+            const headerBuf = new Buffer(parts[0], 'base64');
+            const bodyBuf = new Buffer(parts[1], 'base64');
+            const header = JSON.parse(headerBuf.toString());
+            const body = JSON.parse(bodyBuf.toString());
+
+            if (!header.typ || header.typ !== 'JWT' || !body.email_verified) {
+                return {
+                    isAuth: false
+                };
+            }
+
+            if (body.aud !== settings.CLIENT_ID || body.iss !== 'accounts.google.com') {
+                return {
+                    isAuth: false
+                }
+            }
+
+            if (body.exp < (new Date().getTime() + 1) / 1000) {
+                return {
+                    isAuth: false
+                }
+            }
+
+            return { ...body, isAuth: true };
+        }
+        return {
+            isAuth: false
+        };
     }
     catch (error) {
         console.log(error)
